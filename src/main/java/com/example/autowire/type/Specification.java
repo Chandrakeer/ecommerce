6445c46bf9bf473eed45d5dec7b1e8d8f30cd4d3
Chandra keer <chanchalkeer06@gmail.com>
2026-04-24 19:15:29 +0530

    Add the autowire example

---
+++ src/main/java/com/example/autowire/type/Specification.java
@@ -0,0 +1,34 @@
+package com.example.autowire.type;
+
+public class Specification {
+
+    private String make;
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        System.out.println("model set by type");
+        this.model = model;
+    }
+
+    private String model;
+
+    public String getMake() {
+        return make;
+    }
+
+    public void setMake(String make) {
+        //System.out.println("make called");
+        this.make = make;
+    }
+
+    @Override
+    public String toString() {
+        return "Specification{" +
+                "make='" + make + '\'' +
+                ", model='" + model + '\'' +
+                '}';
+    }
+}
